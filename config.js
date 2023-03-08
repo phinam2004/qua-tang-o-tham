851943b292c63d08de813773ff1df1fa03743c2a
phinam2004 <126351620+phinam2004@users.noreply.github.com>
2023-03-08 14:03:19 +0700

    quâtngotham

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'O Thắm';
+const giftUrl = 'https://www.facebook.com/nguyenthixuan.tham.54';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng O Xuân Thắm';
+const contentCard = 'Chúc O của con 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với O trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'o thắm.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
